0e094047bea538ce5b8e4573afc59058d0bf2737
bsripathy <sripathy.balaji@mavs.uta.edu>
2016-07-19 10:34:01 -0500

    new

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,13 @@
-
 <?php
 namespace App\Http\Controllers; 
   
  use App\Course;
  use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Auth;
  use Illuminate\View\View;
  use App\Http\Requests; 
  use App\User;
- use DB; 
+ use DB;
  use Illuminate\Mail\Message;
  use Illuminate\Support\Facades\Input;
  use Psy\Util\Str;
@@ -17,8 +17,8 @@ namespace App\Http\Controllers;
   
   
      public static function isAdmin(){ 
- 		if (\Auth::guest() == false){ 
- 			$isAdminTrue = \Auth::user()->isadmin; 
+ 		if (\Auth::guest() == false){
+ 			$isAdminTrue = \Auth::user()->isadmin;
  			if ($isAdminTrue === true){ 
  				return true; 
  			} 
--- resources/views/admin/searchCourses.blade.php
+++ resources/views/admin/searchCourses.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<div class="panel panel-default" id="search-results">
+<div class="panel panel-default">
     <div class="panel-heading">Profiles</div>
     <div class="panel-body">
         <a href="/admin/newuser">Add User</a>
@@ -9,11 +9,11 @@
 
 
     @foreach($searchCourses as $c)
-        <div class="panel-body" id="search-results">
+        <div class="panel-body">
             {{$c->name}}, {{$c->email}}
             <form action ="{{route('adminview', ['idedit' => $c->id])}}">
             {{ csrf_field() }}
-            <button  method = "POST"   class="btn btn-primary pull-right"  name = "idedit{{$c->id}}" value = "{{$c->id}}"> View Course Info </button> </form>
+            <button  formmethod="post"   class="btn btn-primary pull-right"  name = "idedit{{$c->id}}" value = "{{$c->id}}"> View Course Info </button> </form>
     @endforeach
         </div>
 </div>
